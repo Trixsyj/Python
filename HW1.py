7cda6bf6fe08ec277d107fdf55833604ff153ed5
Trixsyj <myrsalievaestel@gmail.com>
2025-10-26 22:53:45 +0600

    Homework for lesson1

---
+++ HW1.py
@@ -0,0 +1,99 @@
+#1
+number1 = 26
+number2 = 37
+num1 = number1 + number2
+print(num1) #63
+
+#2
+number1 = 372
+number2 - 164
+num2 = number1/number2
+print(int(num2)) #10
+
+#3
+number1 = 68
+number2 = 19
+num3 = number1 % number2
+print(num3) #11
+
+#4
+float_num1 = 986.45
+float_num2 = 23.55
+float_num3 = 69.2
+num4 = ((float_num1 * float_num2)/float_num3)
+print(num4) #335.7
+
+#5
+float_num1 = 68
+num5 = round(float_num1 ** 3 / 2 , 1)
+print(num5) #157216.0
+
+#6
+import math  
+number1 = 78.45
+number2 = 36.1
+result = number1 - number2
+num6 = math.floor(result)
+print(num6) #42
+
+#7
+import math
+number1 = 5.4
+number2 = 2.8
+num7 = math.ceil(number1 - number2)
+print(num7) #3
+
+#8
+import math
+a = 3
+b = 4
+c = math.sqrt(a**2 + b**2)
+num8 = c
+print(num8) #5.0
+
+#9
+import math
+pos_num = 5
+neg_num = -7
+num9 = pos_num + neg_num
+print(num9) #-2
+
+#10
+temp = 90
+num10 = round((temp - 32) * 5/9, 2)
+print(num10) #32.22
+
+#11
+num11 = (((6+7/12) - (3+17/36)) * 2.5 - (4+1/3) / 0.65)/ ((4/(1/4) - 0.5))
+print(num11) #0.07168
+
+#12
+num12 = ((((2+3/4)/1.1) + (3+1/3))/(2.5 - 0.4 * (3+1/3))) / (5/7) - ((((2+1/6) + 4.5) * 0.375)/(2.75 - (1+1/2)))
+print(num12) #5
+
+#13
+num13 = (11+2/5) + (7+1/2) * ((285.6 / 14) - (1+23/30) + (13/50)) / (24.4 - 10.23)
+print(num13) #21.4
+
+#14
+num14 = ( ((9 - (5 + 3/8)) * ((4 + 5/12) - (4 / (2 + 2/3)))) + (((3/10) - (0.5 / 4)) * (4/7)) ) / ((1/24) + (0.25 / (1 + 1/3)))
+print(num14) #46.57
+
+#15
+percent_value = 5.75  
+percent_rate = 2.5    
+rate_decimal = percent_rate / 100
+num15 = percent_value / rate_decimal
+print(num15) #230
+
+#16
+
+num16 = ((0.16) * (3.2 - 3/40) + (2 + 3/11) * 4.125 / (3 + 3/4)) / ((5 + 1/6) * 0.3 - 0.3 * 4.5 + (1/3) * 0.3)
+result = round((num16) * (40/100))
+print(result)
+
+
+
+
+
+
