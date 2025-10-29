5d13fb8b9da2111004981ae8b1dcad66e3584dc8
Trixsyj <myrsalievaestel@gmail.com>
2025-10-29 20:59:17 +0600

    sixth lesson

---
+++ Seminar2.py
@@ -0,0 +1,121 @@
+#1
+#num1 = int(input(' Enter the number: '))
+#if num1 > 5:
+    #print('True')
+#else:
+    #print('False')
+
+#2
+#num2 = int(input('Enter the number '))
+#if num2 % 2 == 0:
+    #print('True')
+#else:
+    #print('False')
+
+#3
+#a = int(input('Enter the first number: '))
+#b = int(input('Enter the second number: '))
+#if a == 2*b:
+    #print('True')
+#else:
+    #print('False')
+
+
+#4
+#num4 = int(input('Enter the number: '))
+#if num4 % 3 == 0:
+    #print('Yes')
+#else:
+    #print('No')
+
+#5
+#num5 = int(input('Enter the number: '))
+#if num5 %2 == 0:
+    #print(num5)
+#else:
+    #print((num5)+1)
+
+
+#6
+
+#num6 = int(input('Enter the number: '))
+#if num6 > 3 and num6 <= 8:
+    #print('Yes')
+#else:
+    #print('No')
+
+#7
+#num7 = int(input('Enter the number: '))
+#if num7 >= 5 and num7 < 15 and num7 != 10:
+    #print('Yes')
+#else: 
+    #print('No')
+
+#8
+#num8 = int(input('Enter the number: '))
+#if (num8 <= 5) or (num8 > 10):
+    #print('Yes')
+#else:
+    #print('No')
+
+#9
+#num9 = int(input('Enter the number: '))
+#if (num9 > 2) and (num9 <= 6) or (num9 > 10):
+    #print('Yes')
+#else:
+    #print('No')
+
+#10
+#num10 = int(input('Enter the number: '))
+#if (num10 < 4) or (num10 > 10) and (num10 <= 2) or (num10 >= 6):
+    #print('Yes')
+#else:
+    #print('No')
+
+#11
+#num11 = int(input('Enter the number: '))
+#if num11 < 3 or num11 > 5:
+    #print('Yes')
+#else:
+    #print('No')
+
+#12
+#num12 = int(input('Enter the number: '))
+
+#if -3 < num12 <= 6:
+    #print('Yes')
+#else:
+    #print('No')
+
+#13
+#num13 = int(input('Enter the number: '))
+
+#if (num13 >= 4):
+    #print('Yes')
+#else:
+    #print('No')
+
+#14
+#num14 = int(input('Enter the number: '))
+#if (-2 < num14 <= 3) or (x > 5):
+    #print('Yes')
+#else:
+    #print('No')
+
+#15
+#num15 = int(input('Enter the number: '))
+#if (0 < num15 < 4) or (6 <= num15 < 10):
+    #print('Yes')
+#else:
+    #print('No')
+
+#16
+num16 = int(input('Enter the degree '))
+
+if 40 <= num16 <= 45:
+    print('Параметры оптимальны')
+elif num16 < 40:
+    print('Корабль разрушится в атмосфере')
+elif num16 > 45:
+    print('Контролируемый спуск невозможен')
+
---
+++ sixth lesson.py
@@ -0,0 +1,134 @@
+#num1 = input('введите делитель ')
+#num2 = input('введите делимое ')
+
+#try:
+    #num1 = int(num1)
+    #num2 = int(num2)
+    #num3 = num1 / num2
+    #print('Результат деления: ' , num3)
+    #if num3 <= 5:
+        #print('num3 меньше 5')
+    #elif num3 > 5 and num3 <10:
+       #print('num3 больше 5')
+    #else:
+       #print('num3 больше или равно 10')
+#except:
+   #print('Введите корректные числа')
+
+#finally:
+    #print('Блок try завершил выполнение')
+#print ('Завершение программы')
+
+#a = 5
+#b = 6
+#sum1 = a + b
+#print('Sum is ' , sum1)
+
+#word = 'people'
+#text = 'Hello, my name is name'
+#list1 = [2, 4, 5, 6, 'name']
+#search = 'name'
+
+#if search in text:
+    #print(f'{search} is in text')
+#else:
+    #print(f'{search} is not in the text')
+
+#if search in list1:
+    #print(f'{search} is in list {list1}')
+
+#num1 = int(input('Enter the number: '))
+
+#if num1%2 == 0:
+    #print(f'Число {num1} четное')
+#else:
+    #print(f'Число {num1} нечетное')
+
+#Python Easy#1 
+
+#a = int(input('Enter your number '))
+
+#if a > 0:
+    #print(f'The number {a} is positive')
+#else:
+    #print((f'The number {a} is negative. Try again'))
+
+
+#2
+#a = int(input('Enter your number: '))
+
+#if a%2 != 0:
+    #print(f'The number {a} is odd')
+#else:
+    #print(f'The number {a} is even')
+
+#3
+#a = int(input('Enter your number: '))
+
+#if a%2 == 0:
+    #print(f'The number {a} is even')
+#else:
+    #print(f'The number {a} is odd')
+
+#4
+#a = int(input('Enter your number: '))
+#b = int(input('Enter your number: '))
+
+#if a > 2 and b <= 3:
+    #print('True')
+#else:
+    #print('False')
+
+#5
+#a = int(input('Enter your number: '))
+#b = int(input('Enter your number: '))
+
+# if a >= 0 or b < (-2):
+#     print('True')
+# else:
+#     print('False')
+
+# word = 'milk'
+# list = ['apple' , 'banana' , 'milk']
+# if 'milk' in list:
+#     new_word = word.replace('k' , 'ck')
+#     print(new_word)
+
+
+#1
+a = 32
+b = 3.5 
+x = True 
+
+a, b, x = str(a) , str(b) , str(x)
+print(type(a) , type(b), type(x))
+
+#2
+# str_ = input()
+# print(str_)
+
+#3
+# name = input('Write your name: ')
+# surname = input('Write your surname ')
+
+# res = name + ' ' + surname
+# print(res)
+
+#4
+# name = input('Write your name ')
+# score = int(input('Write your score '))
+
+# print(f'Hello,{name}! Your score is {score}')
+
+#5
+# name = input('Write your name ')
+# age  = int(input('Write your name '))
+
+# print(f'My name is {name}, and I am {age} years old')
+
+#6
+
+str1 = 'python is easy'
+str1[0]
+print(str1)
+
